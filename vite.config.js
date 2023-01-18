24cbc7a6668c13adfcaa8f1896b077808aeb45ab
Bence Stuhlmann <bence.stuhlmann@gmail.com>
2023-01-18 19:20:19 +0100

    https and wss

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {VitePWA} from "vite-plugin-pwa";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
+  plugins: [basicSsl(), vue(), VitePWA({
     registerType: "autoUpdate",
     devOptions: {
       enabled: true,
